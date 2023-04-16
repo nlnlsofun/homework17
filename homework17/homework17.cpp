340117b2b3790f1601d58a1512919a2084bd2120
柯仁傑 <@DESKTOP-34DDU9R>
2023-04-16 16:36:03 +0800

    加入專案檔案。

---
+++ homework17/homework17.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    string input;
+    getline(cin, input);
+
+ 
+    transform(input.begin(), input.end(), input.begin(), ::tolower);
+
+    vector<string> words;
+    istringstream iss(input);
+    string word;
+
+  
+    while (iss >> word) {
+        words.push_back(word);
+    }
+
+ 
+    sort(words.begin(), words.end(), [input](const string& a, const string& b) {
+        return input.find(a) < input.find(b);
+        });
+
+
+    words.erase(unique(words.begin(), words.end()), words.end());
+
+ 
+    for (const auto& w : words) {
+        cout << w << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
